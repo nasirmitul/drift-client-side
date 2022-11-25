455a61bb309908ac53bdb3b41aa77ddea46b03b7
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2022-11-26 01:17:37 +0600

    my order data sent from front end

--- src/components/Products/Modal.js
+++ src/components/Products/Modal.js
@@ -4,20 +4,69 @@ import { TiDelete } from 'react-icons/ti';
 import { toast } from 'react-toastify';
 
 const Modal = ({ product, closeModal }) => {
-    const notify = () => toast.success("You have successfully booked this item");
     const { user } = useContext(AuthContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+
+        const user_name = form.user_name.value;
+        const user_email = form.user_email.value;
+        const product_name = form.product_name.value;
+        const product_price = form.product_price.value;
+        const phone_number = form.phone_number.value;
+        const location = form.location.value;
+
+        const myOrder = {
+            user_name,
+            user_email,
+            product_name,
+            product_price,
+            phone_number,
+            location,
+            product_image: product.product_image
+        }
+
+
+        fetch('http://localhost:5000/myOrders', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(myOrder)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if(data.acknowledged){
+                    closeModal();
+                    toast.success("You have successfully booked this item");
+                }
+                console.log(data);
+            })
+
+        // console.log(myOrder);
+
+
+
+
+    }
+
     return (
         <div className='main-modal'>
             <div className={`product-modal`}>
-                <TiDelete onClick={closeModal}></TiDelete>
-                <form action="">
-                    <input type="text" value={user?.displayName} readOnly required />
-                    <input type="text" value={user?.email} readOnly required />
-                    <input type="text" value={product.product_name} readOnly required />
-                    <input type="text" value={product.resale_price} readOnly required />
-                    <input type="tel" placeholder='Phone number' required />
-                    <input type="text" placeholder='Meeting location' required />
-                    <button onClick={notify} className='custom-button'>Submit</button>
+                <div className="modal-title">
+                    <p>Book <span>{product.product_name}</span></p>
+                    <TiDelete className='cross-icon' onClick={closeModal}></TiDelete>
+                </div>
+
+                <form action="" onSubmit={handleSubmit}>
+                    <input type="text" name='user_name' value={user?.displayName} readOnly required />
+                    <input type="text" name='user_email' value={user?.email} readOnly required />
+                    <input type="text" name='product_name' value={product.product_name} readOnly required />
+                    <input type="text" name='product_price' value={product.resale_price} readOnly required />
+                    <input type="tel" name='phone_number' placeholder='Phone number' required />
+                    <input type="text" name='location' placeholder='Meeting location' required />
+                    <button className='custom-button'>Submit</button>
                 </form>
             </div>
         </div>
