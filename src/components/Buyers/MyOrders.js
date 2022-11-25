3843b20a2d75c4ba05bb55ee660cb1c9a252181a
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2022-11-26 02:56:09 +0600

    products my order design complete

--- src/components/Buyers/MyOrders.js
+++ src/components/Buyers/MyOrders.js
@@ -14,8 +14,38 @@ const MyOrders = () => {
     })
 
     return (
-        <div>
-            {myOrders.length}
+
+        <div className='my-orders'>
+            <div className="orders">
+                <div className="order-heading">
+                    <div className="image-name">
+                        <strong className='image'>Image</strong>
+                        <strong className='name'>Name</strong>
+                    </div>
+                    <div className="price-pay">
+                        <strong className='price'>Price</strong>
+                        <strong className='pay'>Action</strong>
+                    </div>
+                </div>
+
+                {
+                    myOrders.map(order =>
+                        <div className="order-body" key={order._id}>
+                            <div className="image-name">
+                                <div className="image">
+                                    <img className='product-image' src={order.product_image} alt="" />
+                                </div>
+
+                                <p className='product-name'>{order.product_name}</p>
+                            </div>
+                            <div className="price-pay">
+                                <p className='price'>{order.product_price}</p>
+                                <p className='pay'>Pay</p>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
         </div>
     );
 };
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,24 +1,37 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { AuthContext } from '../../contexts/UserContext';
 import Modal from './Modal';
+import { FaRegHeart } from 'react-icons/fa';
 
 const Products = () => {
     const products = useLoaderData();
     const [modal, setModal] = useState(null);
-    const closeModal = () =>{
+    const closeModal = () => {
         setModal(null)
     }
     return (
         <div className='container' >
 
-            <div className="products-container" style={{ "display": "grid", "gridTemplateColumns": "repeat(2, 1fr)", "gap": "15px" }}>
+            <div className="products-container">
                 {
                     products.map(product =>
-                        <div key={product._id} >
-                            <p>{product.product_name}</p>
+                        <div className='single-product' key={product._id} >
+                            <div className="picture">
+                                <img src={product.product_image} alt="" />
+                            </div>
+                            <p className='product-name'>{product.product_name}</p>
+                            <p>Location: {product.location}</p>
                             <p>Price: {product.resale_price}</p>
-                            <button className='custom-button' onClick={() => setModal(product)}>Book now</button>
+                            <p>Original Price: {product.original_price}</p>
+                            <p>Used: {product.years_used}</p>
+                            <p>Posted Time: {product.time}</p>
+                            <p>Seller: {product.seller_name}</p>
+                            <div className="actions">
+                                <button className='custom-button' onClick={() => setModal(product)}>Add to basket</button>
+                                <FaRegHeart></FaRegHeart>
+                            </div>
+
                         </div>
                     )
                 }
