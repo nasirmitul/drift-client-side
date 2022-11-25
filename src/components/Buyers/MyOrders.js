7cd2abf0df6603546d1f547458600e7d1ef8550c
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2022-11-26 01:28:34 +0600

    my orders data loaded

--- src/components/Buyers/MyOrders.js
+++ src/components/Buyers/MyOrders.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useQuery } from '@tanstack/react-query'
+import { AuthContext } from '../../contexts/UserContext';
 
 const MyOrders = () => {
+    const { user } = useContext(AuthContext);
+    const { data: myOrders = [] } = useQuery({
+        queryKey: ['seller'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/myOrders/${user?.email}`);
+            const data = await res.json();
+            return data;
+        }
+    })
+
     return (
         <div>
-            <h1>My Orders</h1>
+            {myOrders.length}
         </div>
     );
 };
